7bbd002ddb406d0c34aaff5468c7cb97c00ef624
Omur56 <omur199624@gmail.com>
2025-09-03 01:23:20 +0400

    refresh

--- models/Ad.js
+++ models/Ad.js
@@ -16,7 +16,8 @@ const AdSchema = new mongoose.Schema({
   title: { type: String, required: true },
   link: { type: String }, // burada xəta gəlir
   images: { type: [String], required: true },
-  createdAt: { type: Date, default: Date.now }
+  createdAt: { type: Date, default: Date.now },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true } 
 });
 
 
