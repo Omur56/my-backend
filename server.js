35e3ab8ca3408019e5381d5774db3bbfc3e17e7a
Omur56 <omur199624@gmail.com>
2025-09-16 22:49:19 +0400

    refresh backend

--- server.js
+++ server.js
@@ -149,7 +149,11 @@ app.use("/api", profileRoutes);
 // app.use("/api", otpRoutes);
 
 
+app.use(express.static(path.join(__dirname, "build")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 // test üçün hamıya açmaq istəyirsənsə
 // app.use(cors());
 
