7c2099ca09667c10e050a87600b0db57f00e37cd
Omur56 <omur199624@gmail.com>
2026-01-22 01:19:20 +0400

    refrsh

--- server.js
+++ server.js
@@ -128,7 +128,7 @@ const addWatermark = async (imagePath) => {
 dotenv.config({ path: path.resolve("../.env") });
 
 
-app.use(express.json({ limit: "1mb" }));
+app.use(express.json({ limit: "50mb" }));
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads", "build")));
 app.use("/api", authRoutes);
