74a65393c1142bf4fb540f5b533a6f779e3e715f
Omur56 <omur199624@gmail.com>
2026-02-07 00:50:44 +0400

    refresh

---
+++ models/my-announcements.js
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+import { v4 as uuidv4 } from "uuid";
+const MyAnnouncementSchema = new mongoose.Schema(
+  {
+    title: String,
+    description: String,
+    images: [String],
+    price: String,
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // elan kimə aiddir
+      modelName: String, // hansı modeldə yerləşdirilib (clothing, phone, household, homeGarden)
+      id: { type: String, required: true, default: () => uuidv4() },
+      category: String,
+          brand: String,
+          model: String,
+          ban_type: String,
+          year: String,
+          price: String,
+          location: String,
+      
+          images: [String],
+          mainImage: String,
+          images1: [String],
+      
+          km: String,
+          motor: String,
+          transmission: String,
+          salon: String,
+          barter: String,
+          kredit: String,
+          engine: String,
+          description: String,
+      
+          userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+          },
+      
+          contact: {
+            name: String,
+            email: String,
+            phone: String,
+          },
+      
+          liked: { type: Boolean, default: false },
+          favorite: { type: Boolean, default: false },
+          data: { type: Date, default: Date.now },
+      
+          createdAt: { type: Date, default: Date.now },
+        },
+        { timestamps: true }
+      );
+      
+  
+
+
+const MyAnnouncement = mongoose.model("MyAnnouncement", MyAnnouncementSchema);
+export default MyAnnouncement;
