9d1be01fddf730d860cfd6f31b996a6866007642
Omur56 <omur199624@gmail.com>
2026-05-04 02:44:30 +0400

    refresh

--- server.js
+++ server.js
@@ -5485,7 +5485,7 @@ app.use(express.static(path.join(__dirname, "build")));
 app.use((req, res) => {
   if (req.path.startsWith("/api")) return;
 
-  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(PORT, () => {
